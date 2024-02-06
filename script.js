08111eb80355a420d517e1a14796c1026285db3e
Mohd Shadab <89182232+shadabibrahim@users.noreply.github.com>
2024-02-07 00:50:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let URL =" https://jsonplaceholder.typicode.com/todos/1";
+async function getData() {
+	let data = await fetch(URL);
+	let result = await data.json();
+	console.log(data); 
+	
+}
+getData();
+
